5c1df5d917fb53f743460c73d9cfa578e569f5de
Briggs Elsperger <I3uckwheat@gmail.com>
2017-12-31 16:35:29 -0600

    adds initializer

--- pingMonitor.js
+++ pingMonitor.js
@@ -2,8 +2,20 @@ const fetch = require('node-fetch');
 const fs = require('fs')
 let isLogged = false;
 
-function ping(){
-  fetch("https://www.google.com/")
+initializeLogFile();
+
+setInterval(() => ping("https://www.google.com"), 1000)
+
+/****************************************************************/
+
+function initialize(){
+  if(!logFileExists){
+    console.log("Create Log File")
+  }
+}
+
+function ping(url){
+  fetch(url)
   .then(request => {
     if(isLogged){
       logInfo(`Back Up At: ${Date.now()}\n`)
@@ -23,6 +35,3 @@ function ping(){
 function logInfo(falureValue){
   console.log(falureValue)
 }
-
-
-setInterval(ping, 1000)
