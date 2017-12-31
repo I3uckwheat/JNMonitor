e17e8d44abbfa591af6ebb4d294318c9851da6c7
Briggs Elsperger <I3uckwheat@gmail.com>
2017-12-31 16:26:51 -0600

    adds ability to log up or down time

--- pingMonitor.js
+++ pingMonitor.js
@@ -1,11 +1,28 @@
-var fetch = require('node-fetch');
+const fetch = require('node-fetch');
+const fs = require('fs')
+let isLogged = false;
 
 function ping(){
   fetch("https://www.google.com/")
   .then(request => {
-    console.log("Ping OK")
+    if(isLogged){
+      logInfo(`Back Up At: ${Date.now()}\n`)
+      isLogged = !isLogged;
+    }
+    console.log("Response: OK")
   })
-  .catch(error => console.log("ERROR"));
+  .catch(error => {
+    if(!isLogged){
+      logInfo(`DOWN At: ${Date.now()}`);
+      isLogged = true;
+    }
+    console.log("NetworkDown");
+  });
 }
+
+function logInfo(falureValue){
+  console.log(falureValue)
+}
+
 
 setInterval(ping, 1000)
